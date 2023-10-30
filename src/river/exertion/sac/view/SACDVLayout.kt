ad3b773dd557dfc18f669144cb1dde12b1c0b923
exertionRiver <exertionriver@gmail.com>
2023-10-29 19:45:43 -0600

    adjusting layout

--- src/river/exertion/sac/view/SACDVLayout.kt
+++ src/river/exertion/sac/view/SACDVLayout.kt
@@ -7,41 +7,43 @@ object SACDVLayout {
     fun headerTable() = DVTable(tableTag = "header", cellType = DVLayoutCell.DVLCellTypes.TABLE, panes = mutableListOf(
         DVTextPane().apply {
             this.tag = "AppLabel"
-            this.width = DVPaneType.DVPDimension.TITLE.tag()
-            this.height = DVPaneType.DVPDimension.UNIT.tag()
-        },
-        DVRow()
+            this.width = DVPaneType.DVPDimension.TINY.tag()
+            this.height = DVPaneType.DVPDimension.TINY.tag()
+        }
     ))
 
     fun celestialsTable() = DVTable(tableTag = "celestials", cellType = DVLayoutCell.DVLCellTypes.TABLE, panes = mutableListOf(
         DVTextPane().apply {
             this.tag = "AppLabel2"
-            this.width = DVPaneType.DVPDimension.TITLE.tag()
-            this.height = DVPaneType.DVPDimension.UNIT.tag()
-        },
-        DVRow()
+            this.width = DVPaneType.DVPDimension.TINY.tag()
+            this.height = DVPaneType.DVPDimension.TINY.tag()
+        }
     ))
 
     fun housesTable() = DVTable(tableTag = "houses", cellType = DVLayoutCell.DVLCellTypes.TABLE, panes = mutableListOf(
         DVTextPane().apply {
             this.tag = "AppLabel3"
-            this.width = DVPaneType.DVPDimension.LARGE.tag()
-            this.height = DVPaneType.DVPDimension.UNIT.tag()
+            this.width = DVPaneType.DVPDimension.TINY.tag()
+            this.height = DVPaneType.DVPDimension.TINY.tag()
         }
     ))
 
     fun gridTable() = DVTable(tableTag = "grid", cellType = DVLayoutCell.DVLCellTypes.TABLE, panes = mutableListOf(
         DVTextPane().apply {
             this.tag = "AppLabel4"
-            this.width = DVPaneType.DVPDimension.LARGE.tag()
-            this.height = DVPaneType.DVPDimension.UNIT.tag()
+            this.width = DVPaneType.DVPDimension.TINY.tag()
+            this.height = DVPaneType.DVPDimension.TINY.tag()
         }
     ))
 
     fun dvLayout() = DVLayout(name = "SACDVLayout", layout = mutableListOf(
+        DVTable(tableTag = "layout", cellType = DVLayoutCell.DVLCellTypes.TABLE, panes = mutableListOf(
             headerTable(),
-//            celestialsTable(),
-//            housesTable(),
-//            gridTable()
+            DVRow(),
+            celestialsTable(),
+            DVRow(),
+            housesTable(),
+            gridTable()
+        ))
     ))
 }
